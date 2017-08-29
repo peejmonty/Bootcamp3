beec45f2461019c8cdafb639a0e03092ecbe9d64
Phillip J Viens <phillip.viens@gmail.com>
2017-08-28 23:13:59 -0700

    mostly works fixing siftDown

---
+++ CPSC5003/src/viensp_p2/PatientPriorityQueue.java
@@ -0,0 +1,168 @@
+package viensp_p2;
+
+import java.util.ArrayList;
+
+/**
+ * Hospital triage system implemented using a heap.
+ * @author bc3soln
+ */
+public class PatientPriorityQueue {
+    private ArrayList<Patient> patients; // heap property is always satisfied
+    private int nextPatientNumber;       // num assigned to next added patient
+
+    /**
+     * Creates an empty triage system with no patients.
+     */
+    public PatientPriorityQueue() {
+        this.patients = new ArrayList<Patient>();
+        this.nextPatientNumber = 1;
+    }
+
+    /**
+     * Gets the list of patients currently in the waiting room
+     * @return the list of patients that have not been called
+     */
+    public ArrayList<Patient> getPatientList() {
+        return patients;
+    }
+
+    // TODO: Implement this method and add a javadoc
+    public void addPatient(int priorityCode, String patientName) {
+    	
+    	Patient newPatient = new Patient(priorityCode, 
+    			this.nextPatientNumber, patientName);
+    	
+    	patients.add(newPatient);
+    	siftUp();
+    	this.nextPatientNumber ++;
+    	
+    }
+
+    // TODO: Implement this method and add a javadoc
+    public Patient peek() {
+    	if(this.patients == null)
+        return null;
+    	else
+    		return this.patients.get(0);
+    }
+
+    // TODO: Implement this method and add a javadoc
+    public Patient dequeue() {
+    	if(isEmpty()) {
+    		String message = "Priority Queue is empty.";
+    		throw new RuntimeException(message);
+    	}
+    	else {
+    		Patient val = patients.get(0);
+    		int last = patients.size()-1;
+    		patients.set(0, patients.get(last));
+    		
+    		patients.remove(patients.size()-1);
+    		siftDown();
+    		return val;
+    	}
+    }
+
+    // TODO: Implement this method and add a javadoc
+    public int size() {
+        return patients.size();
+    }
+    
+    public boolean isEmpty() {
+		return (patients.size() == 0);
+	}
+    
+    
+
+    /**
+     * Note: A helper method like this is recommended. It can then be used in
+     * your heap methods to simplify the logic.
+     */
+    private boolean isHigherPriority(int i, int j) {
+        return false;
+    }
+	
+	private void siftUp() {
+		int p = patients.size()-1;
+		while (p != 0) {
+			int parent = (p-1) / 2;
+			if (patients.get(p).getPriorityCode() 
+					>= patients.get(parent).getPriorityCode()) {
+				break;
+			}
+			else {
+				Patient temp = patients.get(parent);
+				patients.set(parent, patients.get(p));
+				patients.set(p, temp);
+				p = parent;
+			}
+		}
+	}
+    
+    public Patient getMin() {
+		if(this.isEmpty())
+			throw new IllegalArgumentException();
+		else
+			return patients.get(0);
+	}
+    
+    private void siftDown() {
+    	int p = 0;
+    	int size = patients.size();
+    	int leftChild = 2 * p + 1;
+    	while (leftChild < size) {
+    		int rightChild = leftChild +1;
+    		int min = leftChild;
+    		
+    		//Is there a right child.
+    		if (rightChild < size) {
+    			//which child is smaller
+    			if(patients.get(rightChild).getPriorityCode()
+    					<= patients.get(leftChild).getPriorityCode() 
+    					&& patients.get(rightChild).getArrivalOrder() 
+    					< patients.get(leftChild).getArrivalOrder()) {
+    				 min = rightChild;
+    			}
+    			else if (patients.get(rightChild).getPriorityCode() 
+    					<= patients.get(leftChild).getPriorityCode() 
+    					&& patients.get(rightChild).getArrivalOrder() 
+    					> patients.get(leftChild).getArrivalOrder())
+    				min = rightChild;
+    			else if (patients.get(rightChild).getPriorityCode() 
+    					> patients.get(leftChild).getPriorityCode() 
+    					&& patients.get(rightChild).getArrivalOrder() 
+    					> patients.get(leftChild).getArrivalOrder())
+    				min = leftChild;
+    			else
+    				min = leftChild;
+    			/*
+    			else if (patients.get(leftChild).getPriorityCode() 
+    					< patients.get(rightChild).getPriorityCode()) {
+    				min = leftChild;
+    			}
+    			else if (patients.get(rightChild).getArrivalOrder() 
+    					< patients.get(leftChild).getArrivalOrder()) {
+    				min = rightChild;
+    			}
+    			else 
+    				min = leftChild;
+    			break;
+    		}
+    		*/
+    		 
+    		
+    		if(patients.get(p).getPriorityCode() 
+    				<= patients.get(min).getPriorityCode()) {
+    			break;
+    		}
+    		else{
+    			Patient temp = patients.get(p);
+    			patients.set(p, patients.get(min));
+    			patients.set(min, temp);
+    		}
+    		
+    		p = min;
+    	}
+    	}
+    }
+}
