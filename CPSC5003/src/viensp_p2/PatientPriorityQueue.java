d1f8b2e3b8aadaac0ebbe247a73455808ced78e1
Phillip J Viens <phillip.viens@gmail.com>
2017-08-29 20:14:56 -0700

    fully functioning product

--- CPSC5003/src/viensp_p2/PatientPriorityQueue.java
+++ CPSC5003/src/viensp_p2/PatientPriorityQueue.java
@@ -1,95 +1,108 @@
+/*
+* CPSC 5003 , Seattle University
+* This is free and unencumbered software released into the public domain.
+*/
 package viensp_p2;
 
 import java.util.ArrayList;
 
 /**
  * Hospital triage system implemented using a heap.
+ * 
  * @author bc3soln
  */
 public class PatientPriorityQueue {
-    private ArrayList<Patient> patients; // heap property is always satisfied
-    private int nextPatientNumber;       // num assigned to next added patient
+	private ArrayList<Patient> patients; // heap property is always satisfied
+	private int nextPatientNumber; // num assigned to next added patient
 
-    /**
-     * Creates an empty triage system with no patients.
-     */
-    public PatientPriorityQueue() {
-        this.patients = new ArrayList<Patient>();
-        this.nextPatientNumber = 1;
-    }
+	/**
+	 * Creates an empty triage system with no patients.
+	 */
+	public PatientPriorityQueue() {
+		this.patients = new ArrayList<Patient>();
+		this.nextPatientNumber = 1;
+	}
 
-    /**
-     * Gets the list of patients currently in the waiting room
-     * @return the list of patients that have not been called
-     */
-    public ArrayList<Patient> getPatientList() {
-        return patients;
-    }
+	/**
+	 * Gets the list of patients currently in the waiting room
+	 * @return the list of patients that have not been called
+	 */
+	public ArrayList<Patient> getPatientList() {
+		return patients;
+	}
 
-    // TODO: Implement this method and add a javadoc
-    public void addPatient(int priorityCode, String patientName) {
-    	
-    	Patient newPatient = new Patient(priorityCode, 
-    			this.nextPatientNumber, patientName);
-    	
-    	patients.add(newPatient);
-    	siftUp();
-    	this.nextPatientNumber ++;
-    	
-    }
+	/**
+	 * adds patients newPatient object.
+	 * @param priorityCode
+	 * @param patientName
+	 */
+	public void addPatient(int priorityCode, String patientName) {
 
-    // TODO: Implement this method and add a javadoc
-    public Patient peek() {
-    	if(this.patients == null)
-        return null;
-    	else
-    		return this.patients.get(0);
-    }
+		Patient newPatient = new Patient(priorityCode, 
+				nextPatientNumber, patientName);
 
-    // TODO: Implement this method and add a javadoc
-    public Patient dequeue() {
-    	if(isEmpty()) {
-    		String message = "Priority Queue is empty.";
-    		throw new RuntimeException(message);
-    	}
-    	else {
-    		Patient val = patients.get(0);
-    		int last = patients.size()-1;
-    		patients.set(0, patients.get(last));
-    		patients.remove(patients.size()-1);
-    		siftDown(0);
-    		return val;
-    	}
-    }
+		patients.add(newPatient);
+		siftUp();
+		nextPatientNumber++;
 
-    // TODO: Implement this method and add a javadoc
-    public int size() {
-        return patients.size();
-    }
-    
-    public boolean isEmpty() {
+	}
+
+	/**
+	 * checks the top of the list.
+	 * @return patients.get(0) the data at index 0
+	 */
+	public Patient peek() {
+		if (this.patients == null)
+			return null;
+		else
+			return this.patients.get(0);
+	}
+
+	/**
+	 * removes the patient at the top of the heap. then calls the siftDown
+	 * method to re organize the heap
+	 * @return val the value at the top of the heap
+	 */
+	public Patient dequeue() {
+		if (isEmpty()) {
+			String message = "Priority Queue is empty.";
+			throw new RuntimeException(message);
+		} else {
+			Patient val = patients.get(0);
+			int last = patients.size() - 1;
+			patients.set(0, patients.get(last));
+			patients.remove(patients.size() - 1);
+			siftDown();
+			return val;
+		}
+	}
+
+	/**
+	 * returns size of the ArrayList
+	 * @return patients.size() arrayList.
+	 */
+	public int size() {
+		return patients.size();
+	}
+
+	/**
+	 * returns true if the arrayList is empty.
+	 * @return patients.size() == 0).
+	 */
+	public boolean isEmpty() {
 		return (patients.size() == 0);
 	}
-    
-    
 
-    /**
-     * Note: A helper method like this is recommended. It can then be used in
-     * your heap methods to simplify the logic.
-     */
-    private boolean isHigherPriority(int i, int j) {
-        return false;
-    }
-	
+	/**
+	 * Sifts up through the array at patients.size()-1
+	 */
 	private void siftUp() {
-		int p = patients.size()-1;
+		int p = patients.size() - 1;
 		while (p != 0) {
-			int parent = (p-1) / 2;
-			if (patients.get(p).getPriorityCode() 
-					>= patients.get(parent).getPriorityCode()) {
+			int parent = (p - 1) / 2;
+			if (patients.get(p).getPriorityCode() >= patients.get(parent).getPriorityCode()) {
 				break;
-			}
-			else {
+			} else {
 				Patient temp = patients.get(parent);
 				patients.set(parent, patients.get(p));
 				patients.set(p, temp);
@@ -97,85 +110,55 @@ public class PatientPriorityQueue {
 			}
 		}
 	}
-    
-    public Patient getMin() {
-		if(this.isEmpty())
-			throw new IllegalArgumentException();
-		else
-			return patients.get(0);
+
+	/**
+	 * Sifts down through the array at given index.
+	 * @param index
+	 */
+	private void siftDown() {
+		int p = 0;
+		int size = patients.size();
+
+		// Is there a left Child.
+		while (2 * p + 1 < size) {
+			int leftChild = 2 * p + 1;
+			int rightChild = leftChild + 1;
+			int min = leftChild;
+
+			// Is there a right child.
+			if (rightChild < size) {
+				// which child is smaller
+				if (patients.get(rightChild).getPriorityCode() 
+						< patients.get(leftChild).getPriorityCode()) {
+					min = rightChild;
+
+				} else if (patients.get(rightChild).getPriorityCode() 
+						> patients.get(leftChild).getPriorityCode()) {
+					min = leftChild;
+				} else {
+					if (patients.get(rightChild).getArrivalOrder() 
+							< patients.get(leftChild).getArrivalOrder()) {
+						min = rightChild;
+					} else {
+						min = leftChild;
+					}
+				}
+			}
+			if (patients.get(p).getPriorityCode() 
+					> patients.get(min).getPriorityCode()) {
+				Patient temp = patients.get(p);
+				patients.set(p, patients.get(min));
+				patients.set(min, temp);
+			} else if (patients.get(p).getPriorityCode() 
+					== patients.get(min).getPriorityCode()
+					&& patients.get(p).getArrivalOrder() 
+					> patients.get(min).getArrivalOrder()) {
+				Patient temp = patients.get(p);
+				patients.set(p, patients.get(min));
+				patients.set(min, temp);
+			}
+
+			p = min;
+		}
 	}
-    
-    private void siftDown(int index) {
-    	int p = index;
-    	int size = patients.size();
-    	while (2 * p + 1 < size) {
-    		int leftChild = 2 * p + 1;
-    		int rightChild = leftChild +1;
-    		int min = p;
-    		
-    		//Is there a right child.
-    		if (rightChild < size) {
-    			//which child is smaller
-    			if(patients.get(rightChild).getPriorityCode()
-    					< patients.get(leftChild).getPriorityCode()) {
-    				if(patients.get(rightChild).getArrivalOrder() 
-    						< patients.get(leftChild).getArrivalOrder()) {
-    					min = rightChild;
-    				}
-    				else if (patients.get(rightChild).getArrivalOrder() 
-    						> patients.get(leftChild).getArrivalOrder()){
-    					min = rightChild;
-    					
-    				}
-    				
-    			}
-    			else if(patients.get(rightChild).getPriorityCode() 
-    					> patients.get(leftChild).getPriorityCode()){
-    				if(patients.get(rightChild).getArrivalOrder() 
-    						< patients.get(leftChild).getArrivalOrder()) {
-    					min = leftChild;
-    				}
-    				else if(patients.get(rightChild).getArrivalOrder() 
-    						> patients.get(leftChild).getArrivalOrder()) {
-    					min = leftChild;
-    				}
-    			}
-    			else {
-    				if(patients.get(rightChild).getArrivalOrder() 
-    						< patients.get(leftChild).getArrivalOrder()){
-    					min = rightChild;
-    					
-    				}
-    				else {
-    					min = leftChild;
-    				}
-    			}
-    			if (patients.get(p).getPriorityCode() 
-    					< patients.get(min).getPriorityCode()) {
-    				min = p;
-    			}
-    			else if(patients.get(p).getPriorityCode() 
-    					== patients.get(min).getPriorityCode()) {
-    				if(patients.get(p).getArrivalOrder() < patients.get(min).getArrivalOrder()) {
-    					min = p;
-    				}
-    				else {
-    					Patient temp = patients.get(p);
-    					patients.set(p, patients.get(min));
-    					patients.set(min, temp);
-    				}
-    				break;
-    			}
-    			else {
-    				Patient temp = patients.get(p);
-    				patients.set(p, patients.get(min));
-    				patients.set(min, temp);
-    			}
-    			break;
-    		
-    		
-    	}
-        	p = min;
-    }
-    }
 }
--- CPSC5003/src/viensp_p2/TriageSystem.java
+++ CPSC5003/src/viensp_p2/TriageSystem.java
@@ -6,171 +6,188 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 /**
- * TODO
- * @author TODO
+ * Main method that calls PatientPriorityQueue class.
  */
 public class TriageSystem {
-    private static final String MSG_WELCOME = "FIXME: Welcome msg goes here.";
-    private static final String MSG_GOODBYE = "FIXME: Goodbye msg goes here.";
-    private static final String MSG_HELP = "FIXME: Help message goes here";
-    private static boolean keepAsking = true;
+	private static final String MSG_WELCOME = "Welcome to the ermergency"
+			+ "\nroom. Please input your type of emergency and your name." 
+			+ "\nFor help type 'help'";
+	private static final String MSG_GOODBYE = "Thanks for visiting!" 
+			+ "Hope we were able to help!";
+	private static final String MSG_HELP = "add <priority-code> <patient-name>\n"
+			+ "            Adds the patient to the triage system.\n"
+			+ "            <priority-code> must be one of the 4 accepted prior" 
+			+ "ity codes:\n"
+			+ "                1. immediate 2. emergency 3. urgent 4. minimal" 
+			+ "\n"
+			+ "            <patient-name>: patient's full legal name (may " 
+			+ "contain spaces)\n"
+			+ "next        Announces the patient to be seen next. Takes " 
+			+ "into account the\n"
+			+ "            type of emergency and the patient's arrival " 
+			+ "order.\n"
+			+ "peek        Displays the patient that is next in line, but " 
+			+ "keeps in queue\n"
+			+ "list        Displays the list of all patients that are " 
+			+ "still waiting\n"
+			+ "            in the order that they have arrived.\n"
+			+ "load <file> Reads the file and executes the command on " 
+			+ "each line\n"
+			+ "help        Displays this menu\n" 
+			+ "quit        Exits the program";
+	private static boolean keepAsking = true;
 
-    /**
-     * Entry point of the program
-     * @param args not used
-     */
-    public static void main(String[] args) {
-        System.out.println(MSG_WELCOME);
+	/**
+	 * Entry point of the program
+	 * @param args not used
+	 */
+	public static void main(String[] args) {
+		System.out.println(MSG_WELCOME);
 
-        Scanner console = new Scanner(System.in);
-        PatientPriorityQueue
-            priQueue = new PatientPriorityQueue();
-        while (keepAsking) {
-            System.out.print("\ntriage> ");
-            String line = console.nextLine();
-            processLine(line, priQueue);
-        }
+		Scanner console = new Scanner(System.in);
+		PatientPriorityQueue priQueue = new PatientPriorityQueue();
+		while (keepAsking) {
+			System.out.print("\ntriage> ");
+			String line = console.nextLine();
+			processLine(line, priQueue);
+		}
+		console.close();
+		System.out.println(MSG_GOODBYE);
+	}
 
-        System.out.println(MSG_GOODBYE);
-    }
+	/**
+	 * Process the line entered from the user or read from the file
+	 * @param line String command to execute
+	 * @param priQueue Priority Queue to operate on
+	 */
+	private static void processLine(String line, PatientPriorityQueue priQueue) {
 
-    /**
-     * Process the line entered from the user or read from the file
-     * @param line     String command to execute
-     * @param priQueue Priority Queue to operate on
-     */
-    private static void processLine(String line,
-                                    PatientPriorityQueue priQueue) {
+		Scanner lineScanner = new Scanner(line); // Scanner to extract words
+		String cmd = lineScanner.next(); // The first is user's command
 
-        Scanner lineScanner = new Scanner(line); // Scanner to extract words
-        String cmd = lineScanner.next();         // The first is user's command
+		// A switch statement could be used on strings, but not all have JDK7
+		if (cmd.equals("help")) {
+			System.out.println(MSG_HELP);
+		} else if (cmd.equals("add")) {
+			addPatient(lineScanner, priQueue);
+		} else if (cmd.equals("peek")) {
+			peekNextPatient(priQueue);
+		} else if (cmd.equals("next")) {
+			dequeueNextPatient(priQueue);
+		} else if (cmd.equals("list")) {
+			showPatientList(priQueue);
+		} else if (cmd.equals("load")) {
+			executeCommandsFromFile(lineScanner, priQueue);
+		} else if (cmd.equals("debug")) {
+			System.out.println(priQueue.toString());
+		} else if (cmd.equals("quit")) {
+			keepAsking = false;
+		} else {
+			System.out.println("Error: unrecognized command: " + line);
+		}
+	}
 
-        // A switch statement could be used on strings, but not all have JDK7
-        if (cmd.equals("help")) {
-            System.out.println(MSG_HELP);
-        } else if (cmd.equals("add")) {
-            addPatient(lineScanner, priQueue);
-        } else if (cmd.equals("peek")) {
-            peekNextPatient(priQueue);
-        } else if (cmd.equals("next")) {
-            dequeueNextPatient(priQueue);
-        } else if (cmd.equals("list")) {
-            showPatientList(priQueue);
-        } else if (cmd.equals("load")) {
-            executeCommandsFromFile(lineScanner, priQueue);
-        } else if (cmd.equals("debug")) {
-            System.out.println(priQueue.toString());
-        } else if (cmd.equals("quit")) {
-            keepAsking = false;
-        } else {
-            System.out.println("Error: unrecognized command: " + line);
-        }
-    }
+	/**
+	 * Reads a text file with each command on a separate line and executes the
+	 * lines as if they were typed into the command prompt.
+	 * @param lineScanner Scanner remaining characters after the command `load`
+	 * @param priQueue priority queue to operate on
+	 */
+	private static void executeCommandsFromFile(Scanner lineScanner, 
+			PatientPriorityQueue priQueue) {
+		// read the rest of the line into a single string
+		String fileName = lineScanner.nextLine().trim();
 
-    /**
-     * Reads a text file with each command on a separate line and executes the
-     * lines as if they were typed into the command prompt.
-     * @param lineScanner Scanner remaining characters after the command `load`
-     * @param priQueue    priority queue to operate on
-     */
-    private static void executeCommandsFromFile(Scanner lineScanner,
-                                                PatientPriorityQueue priQueue) {
-        // read the rest of the line into a single string
-        String fileName = lineScanner.nextLine().trim();
+		try {
+			Scanner file = new Scanner(new File(fileName));
+			while (file.hasNext()) {
+				final String line = file.nextLine();
+				System.out.println("\ntriage> " + line);
+				processLine(line, priQueue);
+			}
+			file.close();
+		} catch (FileNotFoundException e) {
+			System.out.printf("File %s was not found.%n", fileName);
+		}
+	}
 
-        try {
-            Scanner file = new Scanner(new File(fileName));
-            while (file.hasNext()) {
-                final String line = file.nextLine();
-                System.out.println("\ntriage> " + line);
-                processLine(line, priQueue);
-            }
-            file.close();
-        } catch (FileNotFoundException e) {
-            System.out.printf("File %s was not found.%n", fileName);
-        }
-    }
+	/**
+	 * Displays the next patient in the waiting room that will be called.
+	 * @param priQueue priority queue to operate on
+	 */
+	private static void peekNextPatient(PatientPriorityQueue priQueue) {
+		System.out.println("Highest priority patient to be " + "called next: " 
+				+ priQueue.peek().getName());
+	}
 
-    /**
-     * Displays the next patient in the waiting room that will be called.
-     * @param priQueue priority queue to operate on
-     */
-    private static void peekNextPatient(PatientPriorityQueue priQueue) {
-        System.out.println("Highest priority patient to be "
-        		+ "called next: " + priQueue.peek().getName());
-    }
+	/**
+	 * Displays the list of patients in the waiting room.
+	 * @param priQueue priority queue to operate on
+	 */
+	private static void showPatientList(PatientPriorityQueue priQueue) {
+		System.out.println("# patients waiting: " + priQueue.size() + "\n");
+		System.out.println("  Arrival #   Priority Code   "+ "Patient Name\n "
+				+ "+-----------+---------------+--------------+");
+		ArrayList<Patient> patient = priQueue.getPatientList();
+		for (int i = 0; i < patient.size(); i++) {
+			System.out.print("   ");
+			System.out.printf("%3s", patient.get(i).getArrivalOrder());
+			System.out.print("        ");
 
-    /**
-     * Displays the list of patients in the waiting room.
-     * @param priQueue priority queue
-     *  to operate on
-     */
-    private static void showPatientList(PatientPriorityQueue priQueue) {
-        System.out.println("# patients waiting: " + priQueue.size() + "\n");
-        System.out.println("  Arrival #   Priority Code   Patient Name\n" +
-                           "+-----------+---------------+--------------+");
-        ArrayList <Patient> patient = priQueue.getPatientList(); 
-        for(int i = 0; i < patient.size(); i ++) {
-        	System.out.printf("%-13s", patient.get(i).getArrivalOrder());
-        	
-        	int priCode = patient.get(i).getPriorityCode();
-        	if(priCode == 1) {
-        		System.out.printf("%-15s", "immediate");
-        	}
-        	else if(priCode == 2) {
-        		System.out.printf("%-15s", "emergency");
-        	}
-        	else if(priCode == 3) {
-        		System.out.printf("%-15s", "urgent");
-        	}
-        	else
-        		System.out.printf("%-15s", "minimal");
-        	System.out.printf(patient.get(i).getName() + "\n");
-        }
-    }
+			int priCode = patient.get(i).getPriorityCode();
+			if (priCode == 1) {
+				System.out.printf("%-15s", "immediate");
+			} else if (priCode == 2) {
+				System.out.printf("%-15s", "emergency");
+			} else if (priCode == 3) {
+				System.out.printf("%-15s", "urgent");
+			} else
+				System.out.printf("%-15s", "minimal");
+			System.out.printf(patient.get(i).getName() + "\n");
+		}
+	}
 
-    /**
-     * Removes a patient from the waiting room and displays the name on the
-     * screen.
-     * @param priQueue priority queue to operate on
-     */
-    private static void dequeueNextPatient(
-        PatientPriorityQueue priQueue) {
-    	System.out.println("This patient will now be seen: " + priQueue.peek().getName());
-        priQueue.dequeue();
-    }
+	/**
+	 * Removes a patient from the waiting room and displays the name on the
+	 * screen.
+	 * @param priQueue priority queue to operate on
+	 */
+	private static void dequeueNextPatient(PatientPriorityQueue priQueue) {
+		System.out.println("This patient will now be seen: " 
+				+ priQueue.peek().getName());
+		priQueue.dequeue();
+	}
 
-    /**
-     * Adds the patient to the waiting room.
-     * @param lineScanner Scanner with remaining chars after the command
-     * @param priQueue    priority queue to operate on
-     */
-    private static void addPatient(Scanner lineScanner,
-                                   PatientPriorityQueue priQueue) {
-    	
-    	String priority = lineScanner.next().trim();
-    	int priorityCode;
-    	switch (priority) {
-    		case "immediate": 
-    			priorityCode = 1;
-    			break;
-    		case "emergency":
-    			priorityCode = 2;
-    			break;
-    		case "urgent":
-    			priorityCode = 3;
-    			break;
-    		case "minimal":
-    			priorityCode = 4;
-    			break;
-    		default: 
-    			System.out.println("default set to minimal");
-    			priorityCode = 4;
-    	}
-    	String patientName = lineScanner.nextLine();
-    	
-    	
-    	priQueue.addPatient(priorityCode, patientName);
-        System.out.println("Added patient" + patientName + " to the priority system.");
-    }
+	/**
+	 * Adds the patient to the waiting room.
+	 * @param lineScanner Scanner with remaining chars after the command
+	 * @param priQueue priority queue to operate on
+	 */
+	private static void addPatient(Scanner lineScanner, 
+			PatientPriorityQueue priQueue) {
+
+		String priority = lineScanner.next().trim();
+		int priorityCode;
+		switch (priority) {
+		case "immediate":
+			priorityCode = 1;
+			break;
+		case "emergency":
+			priorityCode = 2;
+			break;
+		case "urgent":
+			priorityCode = 3;
+			break;
+		case "minimal":
+			priorityCode = 4;
+			break;
+		default:
+			System.out.println("default set to minimal");
+			priorityCode = 4;
+		}
+		String patientName = lineScanner.nextLine();
+
+		priQueue.addPatient(priorityCode, patientName);
+		System.out.println("Added patient" + patientName + " to the priority system.");
+	}
 }
